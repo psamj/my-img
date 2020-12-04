3f9210e03863b5f49bfd2e9912c3e8511b154406
psamj <tkawjd11@naver.com>
2020-12-04 17:47:50 +0900

    2020-12-04 작업

--- 볼보 리디자인/js/script.js
+++ 볼보 리디자인/js/script.js
@@ -3,16 +3,22 @@ $(document).ready(function(){
         var $this=$(this);
         var index=$this.index();
 
+        if($this.siblings().hasClass("active")){
+            $this.siblings().removeClass("active")  
+            $(".nav-dep-wrap>div").removeClass("active");
+            $(".nav-dep-wrap>div").removeClass("active");
+         }
+
         if($this.hasClass("active")){
             $this.removeClass("active");
-             $(".top-bar>div>ul>li").removeClass("active");
-             $(".top-bar").removeClass("active");
-             $(".nav-2dep").eq(index).removeClass("active");
+             $(".top-bar>div>ul>li>a").removeClass("active");
+             $(".top-bar").removeClass("active");             
+             $(".nav-dep-wrap>div").eq(index).removeClass("active");
           }else{
             $this.addClass("active");
-             $(".top-bar>div>ul>li").addClass("active");
-             $(".top-bar").addClass("active");
-             $(".nav-2dep").eq(index).addClass("active");
+             $(".top-bar>div>ul>li>a").addClass("active");
+             $(".top-bar").addClass("active");             
+             $(".nav-dep-wrap>div").eq(index).addClass("active");
           }
 
     })

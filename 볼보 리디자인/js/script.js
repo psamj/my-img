8e3c51e1c2ad6553ad7d57feb9af9e8d06a6ebd7
psamj <tkawjd11@naver.com>
2020-12-09 02:09:20 +0900

    2020-12-08-1 작업

--- 볼보 리디자인/js/script.js
+++ 볼보 리디자인/js/script.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
         if($this.siblings().hasClass("active")){
             $this.siblings().removeClass("active")  
             $(".nav-dep-wrap>div").removeClass("active");
-            $(".nav-dep-wrap>div").removeClass("active");
          }
 
         if($this.hasClass("active")){
@@ -23,15 +22,42 @@ $(document).ready(function(){
 
     })
 
-    $(".content3>.car-menu>div>a").click(function(){
+    function Box1__init() {
+        var $itemBottomLine = $('.content3>.car-menu>div>span');
+      
+        $('.car-menu >div > a').click(function() {
+          var $this = $(this);
+          var left = $this.position().left;
+          var width = $this.width();
+          $itemBottomLine.css('left', left);
+          $itemBottomLine.css('width', width);
+        });
+      
+        $('.car-menu >div > a').eq(0).click();
+      }
+      
+      Box1__init();
 
-        if($this=="width:33.3%"){
-            
-        }
-        $(".content3>.car-menu>div>p").addClass("active");
+      $(".car-menu >div > a").click(function(){
+        var $this=$(this);
+        var index=$this.index();
 
+        if($this.siblings().hasClass("active")){
+            $this.siblings().removeClass("active")  
+            $(".car-img>.inner-cars>div").removeClass("active");
+         }
 
-    })
+        if($this.hasClass("active")){
+            $(".car-img>.inner-cars>.suv").css({"opacity": "1"})
+
+          }else{
+            $this.addClass("active");
+            $(".car-img>.inner-cars>.suv").css({"opacity": "1"})
+             $(".car-img>.inner-cars>div").eq(index).addClass("active");
+          }
+
+      })
+    
 
 
 })
